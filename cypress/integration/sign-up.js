de26dc2ceb953edbb876e59810312cd122c5b0fe
Abigail  Nyameyie Arko <abigail.arko@amalitech.com>
2023-08-02 10:24:08 +0000

    new message

--- cypress/integration/sign-up.js
+++ cypress/integration/sign-up.js
@@ -1,23 +1,63 @@
-describe('template spec',() =>{
-    beforeEach(()=>{
+import {faker } from "@faker-js/faker";
+describe('sign-up to GHmade',()=>{
+
+    const validEmail = faker.internet.email()
+    
+    const password = faker.internet.password();
+    const invalidEmail = validEmail.replace('@','');  
+    const invalidPassword= password+ '1';
+    const missmatch = ('abigai1234s')
+    it('sign-up to GHmade with valid email and a valid password' , function(){
+        
         cy.visit("https://ghmade.com/market")
         cy.get('[href="/auth/signup"]').click()
+        cy.get('#InputEmail').type(validEmail)
+        cy.get('#InputPassword1').type(password)
+        cy.get('#InputPassword2').type(password)
+        cy.get('#organization').click()
+        cy.get('#signup_button').click()
+    cy.wait(5000)
     })
-    it('sign-up with a valid email and a valid password',()=>{
-        const randomEmail = Math.random().toString(36).substring(2,15)+"@gmail.com"
 
+it('sign-up with an invalid Email and a valid password', function(){
+cy.visit("https://ghmade.com/market")
+cy.get('[href="/auth/signup"]').click()
+cy.get('#InputEmail').type(invalidEmail)
+cy.get('#InputPassword1').type(password)
+cy.get('#InputPassword2').type(password)
+cy.get('#individual').click()
+cy.get('#signup_button').click()
+cy.wait(5000)
+})
 
-        cy.get('#InputEmail').type(randomEmail)
+it('sign-up with a valid Email and a invalid password a the a valid confirm password is valid', function(){
+    cy.visit("https://ghmade.com/market")
+    cy.get('[href="/auth/signup"]').click()
+    cy.get('#InputEmail').type(validEmail)
+    cy.get('#InputPassword1').type(invalidPassword)
+    cy.get('#InputPassword2').type(password)
+    cy.get('#individual').click()
+    cy.get('#signup_button').click()
+    cy.wait(5000)
+})
+it('sign-up with a valid Email, a valid password and an invalid password', function(){
+    cy.visit("https://ghmade.com/market")
+    cy.get('[href="/auth/signup"]').click()
+    cy.get('#InputEmail').type(validEmail)
+    cy.get('#InputPassword1').type(password)
+    cy.get('#InputPassword2').type(invalidPassword)
+    cy.get('#individual').click()
+    cy.get('#signup_button').click()
+    cy.wait(5000)
+})
+it('sign-up to GHmade without selecting an organization' , function(){
         
-        cy.get('#InputPassword1').type('Abigail123&ARKO').invoke('attr','type').should('equal', 'password')
-        cy.get('#InputPassword2').type('Abigail123&ARKO').invoke('attr','type').should('equal', 'password')
-        cy.get('#individual').click().should('be.checked')
-        cy.get('#signup_button').click().should('contain', 'Sign Up')
-        cy.wait(5000)
-
-
-        cy.wait(5000)
-
-
-    })
+    cy.visit("https://ghmade.com/market")
+    cy.get('[href="/auth/signup"]').click()
+    cy.get('#InputEmail').type(validEmail)
+    cy.get('#InputPassword1').type(password)
+    cy.get('#InputPassword2').type(password)
+    cy.get('#signup_button').click()
+cy.wait(5000)
+})
 })
--- cypress/integration/sign-upwithfaker.js
+++ cypress/integration/sign-upwithfaker.js
@@ -6,6 +6,7 @@ describe('sign-up to GHmade',()=>{
     const password = faker.internet.password();
     const invalidEmail = validEmail.replace('@','');  
     const invalidPassword= password+ '1';
+    const missmatch = ('abigai1234s')
     it('sign-up to GHmade with valid email and a valid password' , function(){
         
         cy.visit("https://ghmade.com/market")
@@ -33,7 +34,7 @@ it('sign-up with a valid Email and a missmatch password', function(){
     cy.visit("https://ghmade.com/market")
     cy.get('[href="/auth/signup"]').click()
     cy.get('#InputEmail').type(validEmail)
-    cy.get('#InputPassword1').type(invalidPassword)
+    cy.get('#InputPassword1').type(missmatch)
     cy.get('#InputPassword2').type(password)
     cy.get('#individual').click()
     cy.get('#signup_button').click()
